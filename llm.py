84ec9d454c843c98f546e640cc17d856e1a05427
Pooja <pooja3@pdx.edu>
2024-02-29 16:36:37 -0800

    updated llm.py

--- llm.py
+++ llm.py
@@ -3,24 +3,25 @@ from vertexai.language_models import TextEmbeddingModel, TextGenerationModel
 from langchain_google_vertexai import VertexAI
 from dotenv import load_dotenv
 from google.cloud import aiplatform
+from google.auth import default
 load_dotenv()
 
 aiplatform.init(project=os.environ['PROJECT_ID'], location=os.environ['LOCATION'])
 
 # model = TextEmbeddingModel(model_name="gemini-pro", location=LOCATION)
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cs470-rag-llm.json"
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cs470-rag-llm.json"
 
 # model = TextEmbeddingModel(model_name="gemini-pro")
-# model = TextGenerationModel(model_name="gemini-pro")
+model = TextGenerationModel.from_pretrained("text-bison@001")
 
-model = VertexAI(model_name="text-bison@001")
+#model = VertexAI(model_name="text-bison@001")
 
 
-message = "What are some of the pros and cons of Python as a programming language?"
-model.invoke(message)
+#message = "What are some of the pros and cons of Python as a programming language?"
+#model.invoke(message)
 
 # skipped response
 
-for chunk in model.stream(message):
-    print(chunk, end="", flush=True)
+#for chunk in model.stream(message):
+    #print(chunk, end="", flush=True)
