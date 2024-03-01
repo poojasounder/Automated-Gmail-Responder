c607293d98688df8be1fd91edfb55cf992346cd5
Pooja <pooja3@pdx.edu>
2024-02-29 20:53:06 -0800

    tried to load the documents using DirectoryLoader

--- llm.py
+++ llm.py
@@ -1,9 +1,14 @@
 from langchain_google_vertexai import VertexAI  # type: ignore
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import openai
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import DirectoryLoader
 
+loader = DirectoryLoader('./documents')
+docs = loader.load()
 
-model = VertexAI(model_name="gemini-pro")
-
-message = "What are some of the pros and cons of Python as a programming language?"
-
-print(model.invoke(message))
+print(len(docs))
 
