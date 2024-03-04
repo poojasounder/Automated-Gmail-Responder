b68155b1034f92e9ce186fe851e291aca66142d4
Pooja <pooja3@pdx.edu>
2024-03-03 23:36:57 -0800

    working  with streamlit

--- app.py
+++ app.py
@@ -1,17 +1,94 @@
-"""
-A simple retrieval-augmented generation LLM app.
-"""
-import flask
-import os
-from query import Query
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings.huggingface import HuggingFaceInstructEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css,user_template,bot_template
+# Functions
+# get pdf text
 
-app = flask.Flask(__name__)       # our Flask app
 
-app.secret_key = os.urandom(24)
+def get_pdf_text(pdf_docs):
+    text = "" # initialize a variable called text where the raw text is being stored
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-app.add_url_rule('/',
-                 view_func=Query.as_view('query'),
-                 methods=["GET", 'POST'])
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation(
+        {'question': user_question}
+    )
+    st.write(response)
+# Main function
+def main():
+    
+    load_dotenv()
+    
+    # Just graphical user interface
+    st.set_page_config(page_title="pdx-cs-ask", page_icon=":books")
+    st.write(css,unsafe_allow_html=True)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    st.header("Welcome to pdx-cs-ask!")
+    user_question = st.text_input("Ask a question about the CS department")
+    if user_question:
+        handle_userinput(user_question)
+    
+    st.write(user_template.replace("{{MSG}}", "hello robot"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "hello human"), unsafe_allow_html=True)
+    
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on Process", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                
+                # create vector store with embeddings
+                vectorstore = get_vectorstore(text_chunks)
+                
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+            
+    
+
+if __name__=='__main__':
+    main()
