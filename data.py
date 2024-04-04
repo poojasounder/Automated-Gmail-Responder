500c587d757c10d04f570b74221f43961f7a5a22
Josh Li <josh47tl@gmail.com>
2024-04-04 12:59:40 -0700

    Attempt to implement FAISS

--- data.py
+++ data.py
@@ -1,12 +1,15 @@
 # Replace html2text, it's licensed under GPLv3
-# If we use it we have to license entire project under GPL
+# If we use it we have to license entire project under GPLv3
 import html2text 
 import json
+import numpy as np
+import faiss
 from bs4 import BeautifulSoup
 from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
 from langchain.schema import Document
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from vertexai.language_models import TextEmbeddingModel, TextGenerationModel
-from google.cloud import aiplatform
 
 embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
 generation_model = TextGenerationModel.from_pretrained("text-bison@001")
@@ -15,13 +18,10 @@ generation_model = TextGenerationModel.from_pretrained("text-bison@001")
 TAGS = ["script", "style", "nav", "footer", "aside", "button", "iframe"]
 PATTERN = r"[^\w\s]"
 DEPTH = 3
-SAVE_FILE = "scraped_data.json"
-EMBED_FILE = "embeddings.json"
-GCS_DIR = "gs://cs470-rag-llm-embeddings"
-INDEX_NAME = "rag-llm-vector-store"
-ENDPOINT = "rag-llm-endpoint"
-DEPLOYED_ENDPOINT = "rag-llm-endpoint-deployment"
-DIMENSIONS = 768 # textembedding-gecko returns 768 dimension embedding vectors
+DOC_FILE = "documents.json"
+EMBED_FILE = "embeddings.npy"
+FAISS_FILE = "faiss_index"
+DIMENSION = 768 # textembedding-gecko returns 768 dimension embedding vectors
 
 
 def strip_tags_and_whitespace(html):
@@ -74,88 +74,39 @@ def load_documents_json(filename):
         data = json.load(f)
     return [Document(**doc_dict) for doc_dict in data]
 
+### Functions below cost money ###
 
-### Functions below this cost money because they call embedding model ###
-
-
+# Returns the embedding vector for a string
 def text_embedding(text: str):
     embeddings = embedding_model.get_embeddings([text])
     return embeddings[0].values # get_embeddings returns a list
 
-# Takes in Documents list and writes embeddings as JSON file, returns embeddings as list
-# Clean this up
-def create_embeddings(documents, filename):
-    embeddings = []
-    embeddings_json = []
+# Takes in a Document list and returns a numpy array of embeddings
+def create_document_embedding(documents, filename):
+    embedding_list = []
+    for doc in documents:
+        embedding_list.append(text_embedding(doc.page_content))
+    numpy_array = np.array(embedding_list)
+    np.save(filename, numpy_array)
+    return numpy_array
 
-    for i, doc in enumerate(documents):
-        vector = text_embedding(doc.page_content)
-        embeddings.append(vector)
-        embeddings_json.append({
-            "id": str(i),
-            "embedding": vector
-        })
+def load_document_embeddings(filename):
+    loaded_array = np.load(filename)
+    return loaded_array
 
-    # VertexAI Vector Search Index needs JSONL format, i.e. entries separated by newlines
-    with open(filename, 'w+') as f:
-        for entry in embeddings_json:
-            json.dump(entry, f)
-            f.write('\n')
-    
-    return embeddings
-
-
-### Something below this is messing up, I'm guessing something to do with index endpoint
-#
-#def get_context_id(question_vector):
-#    index_endpoint = aiplatform.MatchingEngineIndexEndpoint(
-#        index_endpoint_name='rag-llm-endpoint-deployment'
-#    )
-#    if index_endpoint:
-#        print(f"Index endpoint resource name: {index_endpoint.name}")
-#        print(
-#            f"Index endpoint public domain name: {index_endpoint.public_endpoint_domain_name}"
-#        )
-#        print("Deployed indexes on the index endpoint:")
-#        for d in index_endpoint.deployed_indexes:
-#            print(f"    {d.id}")
-#    result = index_endpoint.find_neighbors(
-#        deployed_index_id=ENDPOINT,
-#        queries=[question_vector],
-#        num_neighbors=5,
-#        )
-#    print(result)
-#
-#def get_context_from_documents(documents, result_vector):
-#    context = ""
-#    for result in result_vector:
-#        context += (documents[result].page_content + " ")
-#    return context
-#
-#def text_generation_model(**kwargs):
-#    return generation_model.predict(**kwargs).text
-#
-#def answer_question(question, context):
-#    prompt = f""" Answer the question using the provided context. If the answer is
-#              not contained in the context, say "answer not available in context" \n\n
-#                Context: \n {context} \n
-#                Question: \n {question} \n
-#                Answer:
-#              """
-#    return(text_generation_model(prompt=prompt))
+def create_faiss(embeddings):
+    index = faiss.IndexFlatL2(DIMENSION)
+    index.add(embeddings)
+    return index
 
 if __name__ == "__main__":
     website = "https://www.pdx.edu/computer-science/"
     documents = scrape(website, DEPTH)
     documents = remove_extensions(documents)
-    save_documents_json(documents, SAVE_FILE)
-    loaded_documents = load_documents_json(SAVE_FILE)
-    embeddings = create_embeddings(loaded_documents, EMBED_FILE)
-    print(embeddings[0])
-#    question = "What cores are in the grad prep program?"
-#    question_vector = text_embedding(question)
-#    id_vector = get_context_id(question_vector)
-#    print(id_vector)
-#    context = get_context_from_documents(loaded_documents, id_vector)
-#    please_work = answer_question(question, context)
-
+    save_documents_json(documents, DOC_FILE)
+    loaded_documents = load_documents_json(DOC_FILE)
+    embeddings = create_document_embedding(loaded_documents, EMBED_FILE)
+    loaded_embeddings = load_document_embeddings(EMBED_FILE)
+    print(loaded_embeddings)
+    print("Number of embeddings: ", len(loaded_embeddings))
+    db = create_faiss(embeddings)
