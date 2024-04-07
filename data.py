8997b54603eed31012c437e7209a464dacc2c509
Josh Li <josh47tl@gmail.com>
2024-04-07 08:51:20 -0700

    Implement FAISS

--- data.py
+++ data.py
@@ -1,91 +1,125 @@
-# Replace html2text, it's licensed under GPLv3
-# If we use it we have to license entire project under GPLv3
-import html2text 
 import json
-import numpy as np
-import faiss
+import os
 from bs4 import BeautifulSoup
-from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
-from langchain.schema import Document
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from vertexai.language_models import TextEmbeddingModel, TextGenerationModel
+from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
+from langchain.schema import Document
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 
-embedding_model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
-generation_model = TextGenerationModel.from_pretrained("text-bison@001")
+# Definitely don't want CSS file but might find a way to use PDF files
+REMOVE_SITES = [".css", ".pdf"]
+MAX_DEPTH = 4
+SCRAPED_DATA = "documents/scraped_data.json"
+INDEX = "faiss_index"
 
-# tweak these for the PSU site
-TAGS = ["script", "style", "nav", "footer", "aside", "button", "iframe"]
-PATTERN = r"[^\w\s]"
-DEPTH = 3
-DOC_FILE = "documents.json"
-EMBED_FILE = "embeddings.npy"
-FAISS_FILE = "faiss_index"
-DIMENSION = 768 # textembedding-gecko returns 768 dimension embedding vectors
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-MiniLM-L6-v2"
+# )
 
+def files(path):
+    """Returns filenames in given path"""
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
 
-def strip_tags_and_whitespace(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    for tag in soup(TAGS):
-        tag.decompose()
-    return ' '.join(soup.stripped_strings) #stripped_strings removes whitespace
-
-# Removes css and pdf files from Documents list
-def remove_css_and_pdf(documents):
-    extensions_removed = []
-    for doc in documents:
-        source = doc.metadata["source"]
-        # Definitely don't want CSS file but might find a way to use PDF files
-        if not source.endswith(".css") or not source.endswith(".pdf"):
-                extensions_removed.append(doc)
-    return extensions_removed 
-
-# Scrapes given URL and returns list of Document objects
-def scrape(url, max_depth):
-    loader = RecursiveUrlLoader(
-        url=url,
-        max_depth=max_depth,
-        prevent_outside=True,
-        check_response_status=True,
-        extractor=strip_tags_and_whitespace
-    )
-    documents = loader.load()
-    documents = remove_css_and_pdf(documents)
-
-    # Replace this with something else
-    text_maker = html2text.HTML2Text()
-    text_maker.body_width = 0
-
-    for doc in documents:
-        doc.page_content = text_maker.handle(doc.page_content)
-
-    return documents
-
-# Saves list of Documents as JSON file
 def save_documents_json(documents, filename):
+    """Saves list of Documents as JSON file"""
     data = [doc.dict() for doc in documents]
     with open(filename, 'w+') as f:
         json.dump(data, f)
 
-# Reads a JSON file and returns a list of Documents
 def load_documents_json(filename):
+    """Reads a JSON file and returns a list of Documents"""
     with open(filename, 'r') as f:
-        data = json.load(f)
+        data: list = json.load(f)
     return [Document(**doc_dict) for doc_dict in data]
 
-if __name__ == "__main__":
-#    website = "https://www.pdx.edu/computer-science/"
-#    documents = scrape(website, DEPTH)
-#    save_documents_json(documents, DOC_FILE)
-#    embeddings = create_document_embedding(loaded_documents, EMBED_FILE)
-    loaded_documents = load_documents_json(DOC_FILE)
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+def extract_text(html):
+    """Used by RecursiveURLLoader to extract text"""
+    soup = BeautifulSoup(html, "html.parser")
+    article_tag = soup.find("article")
+    if article_tag:
+        return article_tag.get_text(' ', strip=True)
+    div_contents = soup.find("div", id="contents")
+    if div_contents:
+         return div_contents.get_text(' ', strip=True)
+    return ' '.join(soup.stripped_strings)
+
+def remove_css_and_pdf(documents):
+    """Removes css and pdf files from Documents list"""
+    extensions_removed = []
+
+    for doc in documents:
+        source = doc.metadata['source']
+        add = True
+        for extension in REMOVE_SITES:
+            if source.endswith(extension):
+                add = False
+        if add:
+            print(source)
+            extensions_removed.append(doc)
+
+    return extensions_removed 
+
+def scrape(url, max_depth):
+    """Scrapes given URL and returns list of Document objects"""
+    loader = RecursiveUrlLoader(
+        url=url,
+        max_depth=max_depth,
+        timeout=15,
+        prevent_outside=True,
+        check_response_status=True,
+        extractor=extract_text
     )
-    db = FAISS.from_documents(loaded_documents, embeddings)
-    relevant_docs = db.similarity_search(
-        query="What is the University Covid response?",
-        k=5,
-    )
-    print(relevant_docs)
+
+    documents = loader.load()
+    documents = remove_css_and_pdf(documents)
+    save_documents_json(documents, SCRAPED_DATA)
+
+    return documents
+
+# This function probably works, haven't tested because FAISS giving weird results for now
+def create_faiss(scraped_data):
+    """Initializes FAISS with scraped data, ingests pdf files, saves FAISS to disk"""
+    documents = load_documents_json(scraped_data)
+    db = FAISS.from_documents(documents, embeddings)
+    for file_name in files("documents/"):
+        path = f"documents/{file_name}"
+        _, file_type = os.path.splitext(path)
+        if file_type == ".pdf":
+            loader = PyPDFLoader(path)
+            pages = loader.load_and_split()
+            db.add_documents(pages)
+    db.save_local(INDEX)
+
+# Scrape sites
+website = "https://pdx.edu/computer-science/"
+scraped_data = scrape(website, MAX_DEPTH)
+print("Number of pages:", len(scraped_data))
+
+# Create FAISS
+db = FAISS.from_documents(scraped_data, embeddings)
+print("FAISS created")
+
+# Save FAISS to disk
+db.save_local(INDEX)
+print("FAISS saved")
+
+# Load FAISS from disk
+vector_store = FAISS.load_local(INDEX, embeddings, allow_dangerous_deserialization=True)
+print("FAISS loaded")
+
+# Sanity check with similarity_search
+query = "What is the University Coronavirus response?"
+docs = vector_store.similarity_search(query, k=3)
+print(docs)
+print("Number of docs:", len(docs))
+
+# Was going to copy code from test.py in Pooja's data-new branch
+# but sanity check is failing when number of documents increases
+
+# Example:
+# Asking "What is the University coronavirus response?" works when ingesting 10 web pages but not 40
