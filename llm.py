084d0a172810389ee3cf552dcbb169dbaa577ea7
Pooja <pooja3@pdx.edu>
2024-04-15 14:12:13 -0700

    updated

--- llm.py
+++ llm.py
@@ -2,12 +2,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAI,GoogleGenerativeAIEmbeddings
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain.globals import set_llm_cache
-from langchain.cache import InMemoryCache
-from langchain.cache import SQLiteCache
+from langchain.chains.question_answering import load_qa_chain
 
 
 # format the documents
@@ -31,9 +26,9 @@ if __name__ == "__main__":
     #initialized the llm model
     llm = GoogleGenerativeAI(model="gemini-1.0-pro")
     # to use the vectorstore
-    retriever = vectorstore.as_retriever()
+    #retriever=vectorstore.as_retriever()
     email = "Hi ella, Could you go over some infor about the course CS-592?"
-    docs = vectorstore.similarity_search(email,k=3) # Get relevant documents based on the query(success)
+    #docs = vectorstore.similarity_search(email,k=3) # Get relevant documents based on the query(success)
     rag_prompt = '''
     Your role: You are a CS Graduate Advisor at Portland State University
     Your Job: Your job is to respond to emails from students regarding any questions about CS graduate programs
@@ -47,23 +42,15 @@ if __name__ == "__main__":
     you can write an email response saying "Sorry,I am not able to find the provide the answers to your questions"
     If the questions are not related to computer science major, write an email response directing the student to the appropriate department.
     '''
-    prompt = PromptTemplate.from_template(rag_prompt)
-    # invoke the llm model
-    result = run(llm, prompt, email, docs)
-    print(result) # might delete later when integrating with frontend
-    """ qa_chain = RetrievalQA.from_chain_type(
-        llm,
-        retriever=vectorstore.as_retriever(),
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": prompt},
-    ) """
+    prompt = PromptTemplate(template=rag_prompt, input_variables=["context", "email"])
+
+    chain = load_qa_chain(llm, chain_type="stuff", prompt = prompt)
     
-    """ rag_chain = (
-        {"context": retriever | format_docs(docs), "email": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
-    result = rag_chain.invoke(email)
-    set_llm_cache(SQLiteCache(database_path="./langchain.db"))
-    print(result) """
+    retriever = vectorstore.as_retriever()
+    docs = vectorstore.similarity_search(email, k=3)
+    response = chain.invoke ({
+        "input_documents": docs,
+        "email": email
+    }, return_only_outputs=True)
+    
+    print(response["output_text"])
