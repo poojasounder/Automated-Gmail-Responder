afc2d313f44ed376f2023da7c824c85c27eea98d
Pooja <pooja3@pdx.edu>
2024-04-30 13:08:31 -0700

    updated

--- llm.py
+++ llm.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     #initialized the llm model
     llm = ChatOpenAI(model='gpt-3.5-turbo',temperature=0)
     # to use the vectorstore
-    email = "Hi Ella, could you tell me more about the course CS 541? Thank you! Best regards, Julie Nguyen"
+    email = "Hi Ella, could you tell me more about the graduate level machine learning course? Thank you! Best regards, Julie Nguyen"
     #my name is Julie Nguyen and I am interested in the Graduate program. I wanted to know what the requirements are to be admitted into the program. I have a 2.9 GPA and 1 year experience from my internship at Intel. I want to learn more about Computer Science and thrive with my future career in game development. Thank you! Best regards, Julie Nguyen"
     docs = vectorstore.similarity_search(email)
     
